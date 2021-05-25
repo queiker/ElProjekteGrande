19cd5992b2b5ff05318e2a0acccca917c50e219c
Grzegorz Płonka <tradecomp.pl@gmail.com>
2021-05-25 10:06:34 +0200

    First commit of El Projekte Grande

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace El_Projekte_Grande.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
